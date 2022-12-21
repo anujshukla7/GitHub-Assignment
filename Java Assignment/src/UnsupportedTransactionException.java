31216430c1bf3d78f4dc2bae9320d2150391a420
Anuj Shukla <anuj.shukla@nagarro.com>
2022-12-21 16:02:26 +0530

    Add warning msg in the code

--- Java Assignment/src/UnsupportedTransactionException.java
+++ Java Assignment/src/UnsupportedTransactionException.java
@@ -2,7 +2,7 @@
 @SuppressWarnings("serial")
 public class UnsupportedTransactionException extends Exception {
 	public UnsupportedTransactionException() {
-		super("You have entered wrong choice.Enter Valid option .");
+		super("You have enter incorrect information. try with valid data");
 		}
 	public UnsupportedTransactionException(String message) {
 		super(message);
